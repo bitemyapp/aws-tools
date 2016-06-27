3ff28789fc24e8ae9f0dc79a7bae422868fe5d9b
Trevor Basinger <trevor.basinger@gmail.com>
2016-06-27 11:53:53 -0500

    Added IP Permissions

--- src/Commander/EC2/SecurityGroup.hs
+++ src/Commander/EC2/SecurityGroup.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts  #-}
 module Commander.EC2.SecurityGroup where
 
 import Control.Lens
+import Control.Monad.Reader
 
 import Data.Maybe
 
@@ -11,7 +13,15 @@ import Network.AWS.EC2
 import Data.Text (Text)
 import qualified Data.Text as Text
 
+import Commander.Types
+
 commanderSGName = "CommanderSG"
+
+openInboundPort :: Text -> Int -> IPPermission
+openInboundPort c p = 
+  ipPermission "tcp" & ipFromPort ?~ p
+                     & ipToPort   ?~ p
+                     & ipIPRanges .~ [ipRange c]
 
 checkToSeeIfSecurityGroupNameExists :: (MonadAWS m) => Text -> m (Maybe SecurityGroup)
 checkToSeeIfSecurityGroupNameExists name = do
@@ -22,14 +32,29 @@ checkToSeeIfSecurityGroupNameExists name = do
   where
     maybeGetMatching = listToMaybe . filter (\sg -> name == sg ^. sgGroupName)
 
-createCommanderSecurityGroup :: (MonadAWS m) => Text -> m Text
+createCommanderSecurityGroup :: (MonadAWS m, MonadReader AppConfig m) => Text -> m Text
 createCommanderSecurityGroup name = do
+  cidr     <- view $ configFile . awsSGCidr
+  port     <- view $ configFile . awsSGPort
   response <- send $ createSecurityGroup name ""
-  return $ response ^. csgrsGroupId
 
-getCommanderSecurityGroupId :: (MonadAWS m) => m Text
+  let groupId       = response ^. csgrsGroupId
+      ipPermissions = openInboundPort cidr port : []
+
+  -- Attach permissions
+                                       
+  send $ authorizeSecurityGroupIngress & asgiGroupId       ?~ groupId
+                                       & asgiFromPort      ?~ port
+                                       & asgiToPort        ?~ port
+                                       & asgiCIdRIP        ?~ cidr
+                                       & asgiIPProtocol    ?~ "tcp"
+                                       & asgiIPPermissions .~ ipPermissions
+
+  return groupId
+
+getCommanderSecurityGroupId :: (MonadAWS m, MonadReader AppConfig m) => m Text
 getCommanderSecurityGroupId = createGroupIfNotExists =<< checkToSeeIfSecurityGroupNameExists commanderSGName 
   where
-    createGroupIfNotExists :: (MonadAWS m) => Maybe SecurityGroup -> m Text
+    createGroupIfNotExists :: (MonadAWS m, MonadReader AppConfig m) => Maybe SecurityGroup -> m Text
     createGroupIfNotExists (Just sg) = return $ sg ^. sgGroupId
     createGroupIfNotExists _         = createCommanderSecurityGroup commanderSGName
